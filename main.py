1dfbb448b768ca1c8a647d5ae6e438ae291da364
seyken799 <73948604+seyken799@users.noreply.github.com>
2021-03-03 16:08:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,88 @@
-import sys as system
 
-print(system.version)
-
-a=2
 print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+#zad2
+a = 10
+b = 5
+c = 2
+
+dodawanie = a + b
+odejmowanie = a - c
+mnozenie = a * c
+dzielenie = a/b
+dzielenie_calkowite = b // c
+potega = c ** b
+reszta = a%b
+
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(dzielenie)
+print(dzielenie_calkowite)
+print(potega)
+print(reszta)
+
+#zad3
+
+a = 10
+a += 1
+print(a)
+
+a = 9
+a -= 1
+print(a)
+
+a = 5
+a *= 1
+print(a)
+
+a = 6
+a /= 3
+print(a)
+
+a = 10
+a **= 2
+print(a)
+
+a = 10
+a %= 2
+print(a)
+
+#zad4
+from math import *
+print(e ** 10)
+
+print(log(5+sin(radians(8)**2)**(1/6)))
+
+print(floor(3.55))
+
+print(ceil(4.80))
+
+#zad5
+imie = "BLAZEJ"
+nazwisko = "JURCZAK"
+print(imie.capitalize() + " " + nazwisko.capitalize())
+
+#zad6
+tekst = "malowana la la, la la la la la"
+print(tekst.count("la"))
+
+#zad7
+lancuch = "technik informatyk"
+print(lancuch[1])
+print(lancuch[-1])
+
+#zad8
+tekst = "malowana la la, la la la la la"
+print(tekst.split())
+
+#zad9
+a = "napis"
+b = 1.41
+c = 0x123AF
